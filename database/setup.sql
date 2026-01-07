efb9e47ae74e8b705bfb96110d87f80770d8cd72
Gutenberg Farias Pimentel Júnior <62069916+gutenbergfpj@users.noreply.github.com>
2026-01-07 12:04:59 +0000

    Create users and stories tables in setup.sql
    Added users and stories tables with appropriate fields and constraints.

--- database/setup.sql
+++ database/setup.sql
@@ -1,5 +1,16 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Habilita extensao de criacao de uuid auto generated
 
-CREATE TABLE IF NOT EXISTS "historias" (
-    "id" UUID DEFAULT uuid_generate_v4() PRIMARY KEY
+CREATE TABLE IF NOT EXISTS users (
+    id   UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    username   TEXT NOT NULL,
+    email      TEXT NOT NULL,
+    pw_hash    CHARACTER[256] NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS stories (
+    id          UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    creator_id  BIGINT NOT NULL REFERENCES users(id),
+    created_at  TIMESTAMPTZ DEFAULT now(),
+    title       TEXT NOT NULL,
+    contents    TEXT NOT NULL
 );
