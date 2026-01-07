d364a24a2ab75e80f8e41aa3ca9d5021c610f15f
Gutenberg Farias Pimentel Júnior <62069916+gutenbergfpj@users.noreply.github.com>
2026-01-07 14:29:21 +0000

    Update UUID generation method in setup.sql
    Replaced uuid_generate_v4() with gen_random_uuid() for UUID generation. PostgreSQL's own UUID generator is recommended from version 13 onwards.

--- database/setup.sql
+++ database/setup.sql
@@ -1,14 +1,12 @@
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp"; -- Habilita extensao de criacao de uuid auto generated
-
 CREATE TABLE IF NOT EXISTS users (
-    id   UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id   UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     username   TEXT NOT NULL,
     email      TEXT NOT NULL,
     pw_hash    CHARACTER[256] NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS stories (
-    id          UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+    id          UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     creator_id  BIGINT NOT NULL REFERENCES users(id),
     created_at  TIMESTAMPTZ DEFAULT now(),
     title       TEXT NOT NULL,
